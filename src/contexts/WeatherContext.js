5916a1b0576d4c44699ed6f40604a1f1a02f4c05
Rafsani <ahrafsani@gmail.com>
2022-07-13 23:50:38 +0600

    update file

--- src/contexts/WeatherContext.js
+++ src/contexts/WeatherContext.js
@@ -19,7 +19,7 @@ const WeatherContextProvider = ({children})=>{
     const getLocationWeatherByUserInput = async(location)=>{
         try{
             dispatch({type:"GET_WEATHER_REQUEST"});
-            const {data} = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${process.env.REACT_APP_API_KEY}`);
+            const {data} = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${location}&appid=${process.env.REACT_APP_API_KEY}`);
             const weather = await axios.get(`https://api.openweathermap.org/data/2.5/onecall?lat=${(data[0].lat).toString()}&lon=${(data[0].lon).toString()}&units=metric&exclude=minutely&appid=${process.env.REACT_APP_API_KEY}`);
             dispatch({type:"GET_WEATHER_REQUEST_SUCESS",payload:weather.data});
         }
@@ -44,11 +44,11 @@ const WeatherContextProvider = ({children})=>{
         try{
             dispatch({type:"GET_LOCATION_REQUEST"});
             if(typeof(data)==="object"){
-                const location = await axios.get(`http://api.openweathermap.org/geo/1.0/reverse?lat=${(data.lat).toString()}&lon=${(data.lon).toString()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`);
+                const location = await axios.get(`https://api.openweathermap.org/geo/1.0/reverse?lat=${(data.lat).toString()}&lon=${(data.lon).toString()}&limit=5&appid=${process.env.REACT_APP_API_KEY}`);
                 console.log(location.data[0]);
                 dispatch({type:"GET_LOCATION_REQUEST_SUCESS",payload:location.data[0]});
             }else{
-                const location = await axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${data}&appid=${process.env.REACT_APP_API_KEY}`);
+                const location = await axios.get(`https://api.openweathermap.org/geo/1.0/direct?q=${data}&appid=${process.env.REACT_APP_API_KEY}`);
                 console.log(location);
                 dispatch({type:"GET_LOCATION_REQUEST_SUCESS",payload:location.data[0]});
             }
